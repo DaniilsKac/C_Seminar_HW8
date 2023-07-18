37d71ebf72ba7cc02dd35041584cdc52c8a7f350
Daniils_Kackins <daniil.kackin@gmail.com>
2023-07-18 14:46:16 +0100

    Добавил задачу номер 58

---
+++ Task_003/Program.cs
@@ -0,0 +1,82 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+Console.Clear();
+
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите минимальное значение: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите максимальное значение: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+int[,] firstArray = GetMatrix(rows, columns, minValue, maxValue);
+int[,] secondArray = GetMatrix(rows, columns, minValue, maxValue);
+int[,] finalArray = new int[rows, columns];
+Console.WriteLine("Первая матрица: ");
+PrintMatrix(firstArray);
+Console.WriteLine("\nВторая матрица: ");
+PrintMatrix(secondArray);
+
+MultiplyMatrix(firstArray, secondArray, finalArray);
+Console.WriteLine($"\nПроизведение двух матриц: ");
+PrintMatrix(finalArray);
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+int[,] GetMatrix(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+    for (int i = 0; i < resultMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultMatrix.GetLength(1); j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < firstMartrix.GetLength(1); k++)
+            {
+                sum += firstMartrix[i, k] * secomdMartrix[k, j];
+            }
+            resultMatrix[i, j] = sum;
+        }
+    }
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
