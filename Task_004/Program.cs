1fdc1a5257ae301a519feaab595f33b11ef0e393
DaniilsKac <136686055+DaniilsKac@users.noreply.github.com>
2023-07-21 10:49:07 +0100

    Update Program.cs

--- Task_004/Program.cs
+++ Task_004/Program.cs
@@ -13,8 +13,67 @@ int rows = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите количество столбцов: ");
 int columns = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите минимальное значение: ");
-int minValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество страниц: ");
+int pages = Convert.ToInt32(Console.ReadLine());
+ 
+ int array = new int [columns, rows, pages];
+ GetMatrix(array);
+ PrintMatrix(array);
 
-Console.Write("Введите максимальное значение: ");
-int maxValue = Convert.ToInt32(Console.ReadLine());
+//-----------------------------------------------------------------------------
+
+void PrintMatrix(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"X({i})  Y({j}) ");
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"Z({k})={array[i,j,k]}; ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+//-----------------------------------------------------------------------------
+
+void GetMatrix(int[,,] array)
+{
+    int[] temp = new int[array.GetLength(0) * array.GetLength(1) * array.GetLength(2)];
+    int number;
+    for (int i = 0; i < temp.GetLength(0); i++)
+    {
+        temp[i] = new Random().Next(-100, 100);
+        number = temp[i];
+        if (i >= 1)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                while (temp[i] == temp[j])
+                {
+                    temp[i] = new Random(), Next(-100, 100);
+                    j = 0;
+                    number = temp[i];
+                }
+                number = temp[i];
+            }
+        }
+    }
+    int count = 0;
+    for (int a = 0; a < array.GetLength(0); a++)
+    {
+        for (int b = 0; b < array.GetLength(1); b++)
+        {
+            for (int c = 0; c < array.GetLength(2); c++)
+            {
+                array[a,b,c] = temp[count];
+                count++;
+            }
+        }
+    }
+}
+//-----------------------------------------------------------------------------
